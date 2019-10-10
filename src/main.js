7b15999ccf0026f8999982e2255f570bacbeaf70
Bo Nguyen <nguyenbo2001@gmail.com>
2019-10-10 16:27:19 +0700

    finish

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import StarRating from 'vue-star-rating'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.component('star-rating', StarRating);
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
